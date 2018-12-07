1b07cd5caa68d86b12687dacf6738962e5e08972
SUPRIYAM23 <supriya.masani@gmail.com>
2018-12-07 13:43:10 -0500

    initial commit1

---
+++ eCP/src/main/java/eCommProject/eCP/App.java
@@ -0,0 +1,13 @@
+package eCommProject.eCP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
